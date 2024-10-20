b11bc94249ff331e9707ac4dcbc79cb6749837ef
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-10-20 11:04:23 -0400

    [53] [Time Beats: 99.40%] [Memory Beats: 23.88%] - LeetPush

---
+++ 53-Maximum-Subarray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #you have to select the max subarray in nums, not selecting anything is not valid. If it was they would have told us in the specs. 
+        maxSum = nums[0]
+        curSum = nums[0]
+
+        for i in range(1, len(nums)):
+            if curSum < 0:
+                curSum = nums[i]
+            else:
+                curSum += nums[i]
+            maxSum = max(maxSum, curSum)
+        return max(maxSum, curSum)
+        
