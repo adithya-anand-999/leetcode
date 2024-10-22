032a5dc20209916f3155cb913e8f526cbd7f7add
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-10-22 11:11:30 -0400

    [20] [Time Beats: 100.00%] [Memory Beats: 94.28%] - LeetPush

--- 20-Valid-Parentheses.py
+++ 20-Valid-Parentheses.py
@@ -1,14 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        opens = ['(','[','{']
+        closes = [')',']','}']
         stack = []
-        opens = ['(', '{', '[']
-        closes = [')', '}', ']']
 
         for c in s:
-            if c in opens:
-                stack.append(c)
-            elif c in closes:
-                #if trying to pop you have to make sure the stack is not empty. 
-                if not stack or opens[closes.index(c)] != stack.pop():
+            if c in opens: stack.append(c)
+            else:
+                if not stack or stack.pop() != opens[closes.index(c)]:
                     return False
         return not stack
