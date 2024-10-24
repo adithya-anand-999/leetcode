3b4cd51dd05438e68cace508b88b39b93d753f12
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-10-24 18:51:46 -0400

    [235] [Time Beats: 91.54%] [Memory Beats: 47.16%] - LeetPush

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
@@ -7,26 +7,11 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        #use the fact that the tree is a binary search tree, meaning child to the left of root is less than root, the child to right of root is more than root. 
-
-        #Running into issue where I am returning None, even though it looks like I should be returning the node itself. 
-        pV,qV = p.val,q.val
-
         def help(node):
-            #No need for base case as we are gaurenteed to return a value 
-            # if not node:
-            #     return
-            nV = node.val
-            
-            # if nV == pV or nV == qV or ((pV < nV and qV > nV) or (pV > nV and qV < nV)):
-            #     return node
-            if nV < pV and nV < qV:
-                print(1)
+            if p.val > node.val and q.val > node.val:
                 return help(node.right)
-            elif nV > pV and nV  > qV:
-                print(2)
+            elif p.val < node.val and q.val < node.val:
                 return help(node.left)
             else:
-                print(3)
                 return node
         return help(root)
