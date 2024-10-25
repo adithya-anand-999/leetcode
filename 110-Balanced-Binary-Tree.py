33503f3f7f678f05fa052399019aeb5b0468ac86
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-10-25 11:43:32 -0400

    [110] [Time Beats: 92.35%] [Memory Beats: 5.16%] - LeetPush

--- 110-Balanced-Binary-Tree.py
+++ 110-Balanced-Binary-Tree.py
@@ -6,13 +6,13 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
         def help(node) -> Tuple[bool,int]:
             if not node:
                 return (True, 0)
-            left, right = help(node.left), help(node.right)
-
+            left,right = help(node.left),help(node.right)
             balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
 
-#the issue is in the part 1 + max(left[0], right[0]), the 0th index are the bool values, you know they should be the height values. Silly mistake. 
-            return (balanced, 1 + max(left[1], right[1]))
+            return (balanced, 1+max(left[1], right[1]))
         return help(root)[0]
+        
