7269523f905094e30a1682f669f496b48169a31e
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-10-28 09:37:27 -0400

    [235] [Time Beats: 85.79%] [Memory Beats: 88.61%] - LeetPush

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
@@ -8,10 +8,10 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         def help(node):
+            if node.val > p.val and node.val > q.val:
+                return help(node.left)
             if node.val < p.val and node.val < q.val:
                 return help(node.right)
-            elif node.val > p.val and node.val > q.val:
-                return help(node.left)
             else:
                 return node
         return help(root)
