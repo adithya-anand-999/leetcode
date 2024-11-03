cbbf8b577621da051dc70d3b18e5a71f0c1c9d3e
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-02 23:51:03 -0400

    [70] [Time Beats: 100.00%] [Memory Beats: 40.23%] - LeetPush

--- 70-Climbing-Stairs.py
+++ 70-Climbing-Stairs.py
@@ -1,21 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # count = 0
-        # def help(n):
-        #     nonlocal count
-        #     if n < 0: return
-        #     if n == 0: count+=1
-        #     help(n-1)
-        #     help(n-2)
-        # help(n)
-        # return 
-        
-        if n == 1:return 1
+        if n == 1: return 1
+        p1 = 1
+        p2 = 2
 
-        stepP1 = 1
-        stepP2 = 2
-        res = 2
         for i in range(2,n):
-            res = stepP1 + stepP2
-            stepP1, stepP2 = stepP2, res
-        return res
+            p1,p2 = p2,p1+p2
+        return p2
