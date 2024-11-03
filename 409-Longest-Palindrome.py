ffaad913eed9dbebb4049c01dc45f9287159d213
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-02 23:46:53 -0400

    [409] [Time Beats: 16.16%] [Memory Beats: 40.82%] - LeetPush

--- 409-Longest-Palindrome.py
+++ 409-Longest-Palindrome.py
@@ -1,20 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        freq = {}
+        count = defaultdict(int)
+        for c in s:count[c]+=1
+
         res = 0
         seenOne = False
-
-        for c in s:
-            if c not in freq:
-                freq[c] = 1
-            else:
-                freq[c]+=1
-        
-        for cnt in freq.values():
-            if cnt%2 == 0:
-                res+=cnt
+        for v in count.values():
+            if v%2 == 0:
+                res+=v
             else:
                 seenOne = True
-                res+=cnt-1
-        if seenOne: res+=1
+                res+=(v-1)
+                
+        if seenOne:res+=1
         return res
