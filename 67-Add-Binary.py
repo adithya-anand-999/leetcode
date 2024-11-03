86686f2f8a46418072c4d4c33b0d24d4b087435b
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-02 23:32:32 -0400

    [67] [Time Beats: 18.78%] [Memory Beats: 13.27%] - LeetPush

--- 67-Add-Binary.py
+++ 67-Add-Binary.py
@@ -1,27 +1,26 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        a,b = list(a),list(b)
+        la = len(a)
+        lb = len(b)
+        if la < lb:
+            a = ['0']*abs(la-lb) + a
+        elif lb < la:
+            b = ['0']*abs(la-lb) + b
+        print(a,b)
+
         carry = 0
-        a = list(a)
-        b = list(b)
-        longer,shorter = (a,b) if len(a) > len(b) else (b,a)
-
-        shorter = [0]*(len(longer)-len(shorter)) + shorter
-
-        for i in range(len(shorter)-1,-1,-1):
-            val = int(shorter[i]) + int(longer[i]) + carry
-            if val == 0:
-                shorter[i] = '0'
+        for i in range(len(a)-1, -1, -1):
+            sum = carry + int(a[i]) + int(b[i])
+            if sum == 0:
+                b[i] = '0'
+            elif sum == 1:
+                b[i] = '1'
                 carry = 0
-            elif val == 1:
-                shorter[i] = '1'
-                carry = 0
-            elif val == 2:
-                shorter[i] = '0'
+            elif sum == 2:
+                b[i] = '0'
                 carry = 1
             else:
-                shorter[i] = '1'
-                carry = 1  
-        shorter = ''.join(shorter)
-        if carry == 1: shorter = '1'+shorter
-        return shorter
-
+                b[i] = '1'
+                carry = 1
+        return ''.join(b) if carry == 0 else ''.join(['1']+b)
