50979afd3af825ef3e0139f413bf88e90eada986
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-02 20:45:10 -0400

    [125] [Time Beats: 90.99%] [Memory Beats: 57.20%] - LeetPush

--- 125-Valid-Palindrome.py
+++ 125-Valid-Palindrome.py
@@ -1,28 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        valid = 'abcdefghijklmnopqrstuvwxyz1234567890'
+        # print(s)
+        valid = 'abcdefghijklmnopqrstuvwxyz0123456789'
+        conFlag = False
 
         l,r = 0,len(s)-1
         while l<r:
-            vl,vr = s[l] in valid, s[r] in valid
-            if not vl and not vr:
+            if s[l] not in valid:
                 l+=1
+                conFlag = True
+            if s[r] not in valid:
                 r-=1
+                conFlag = True
+            if conFlag:
+                conFlag = False
                 continue
-            if not vl:
-                l+=1
-                continue
-            if not vr:
-                r-=1
-                continue
-            
-            if s[l] != s[r]:
-                return False
+            if s[l] != s[r]:return False
             l+=1
             r-=1
         return True
-
-
-
-        
