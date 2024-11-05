46dd58cd5e806b82ee364fb2bb26ed51c1f94381
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-05 08:58:30 -0500

    [67] [Time Beats: 46.19%] [Memory Beats: 37.68%] - LeetPush

--- 67-Add-Binary.py
+++ 67-Add-Binary.py
@@ -1,26 +1,25 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         a,b = list(a),list(b)
-        la = len(a)
-        lb = len(b)
-        if la < lb:
-            a = ['0']*abs(la-lb) + a
-        elif lb < la:
-            b = ['0']*abs(la-lb) + b
-        print(a,b)
-
+        diff = abs(len(a) - len(b))
+        if len(a) < len(b):
+            a = ['0']*diff + a
+        else:
+            b = ['0']*diff + b
+        
         carry = 0
-        for i in range(len(a)-1, -1, -1):
-            sum = carry + int(a[i]) + int(b[i])
+        for i in range(len(b)-1,-1,-1):
+            sum = int(a[i]) + int(b[i]) + carry
             if sum == 0:
-                b[i] = '0'
+                a[i] = '0'
             elif sum == 1:
-                b[i] = '1'
+                a[i] = '1'
                 carry = 0
             elif sum == 2:
-                b[i] = '0'
+                a[i] = '0'
                 carry = 1
             else:
-                b[i] = '1'
+                a[i] = '1'
                 carry = 1
-        return ''.join(b) if carry == 0 else ''.join(['1']+b)
+        # if carry == 1: a = + a
+        return ''.join(a) if carry == 0 else str(carry)+''.join(a)
