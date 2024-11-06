dadab4092b9890506d7f7d256d4f770aeb4b3bad
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-06 16:52:17 -0500

    [133] [Time Beats: 17.45%] [Memory Beats: 99.03%] - LeetPush

--- 133-Clone-Graph.py
+++ 133-Clone-Graph.py
@@ -11,29 +11,17 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node: return None
         visited = {}
-        queue = deque([node])
         visited[node] = Node(node.val, [])
+        queue = deque([node])
 
         while queue:
             curr = queue.popleft()
+
             for n in curr.neighbors:
                 if n not in visited:
+                    #n has not been seen before, so we need to make its copy 
                     visited[n] = Node(n.val, [])
+                    #we have also not seen n's neighbors before
                     queue.append(n)
                 visited[curr].neighbors.append(visited[n])
         return visited[node]
-
-        # first = True
-        # head = None
-
-        # while queue:
-        #     curr = queue.pop()
-        #     currNode = Node(curr.val, curr.neighbors)
-        #     if first:
-        #         head = currNode
-        #         first = False
-        #     visited.append(curr.val)
-        #     for n in curr.neighbors:
-        #         if n not in visited:
-        #             queue.append(n)
-        # return head
