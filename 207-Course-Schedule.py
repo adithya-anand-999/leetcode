6424fd3fc4c1033aed92c90e7057143f13636b44
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-07 21:08:46 -0500

    [207] [Time Beats: 93.20%] [Memory Beats: 19.48%] - LeetPush

--- 207-Course-Schedule.py
+++ 207-Course-Schedule.py
@@ -1,27 +1,11 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # prereqs = defaultdict(list)
-
-        # for target,prereq in prerequisites:
-        #     if target == prereq: return False
-        #     stack = [prereq]
-        #     while stack:
-        #         curr = stack.pop()
-        #         if curr in prereqs:
-        #             if target in prereqs[curr]: return False
-        #             stack += prereqs[curr]
-
-        #     # if prereq in prereqs and target in prereqs[prereq]:
-        #     #     return False
-        #     prereqs[target].append(prereq)
-        # return True
-            # else:
         prereqs = defaultdict(list)
-        for target,prereq in prerequisites:
-            prereqs[target].append(prereq)
+        for crs,pre in prerequisites:
+            prereqs[crs].append(pre)
+        
         visited = set()
         path = set()
-
         def dfs(node):
             if node in visited: return True
             if node in path: return False
@@ -32,9 +16,7 @@ class Solution:
             path.remove(node)
             visited.add(node)
             return True
-        
+
         for crs in range(numCourses):
             if not dfs(crs): return False
         return True
-
-
