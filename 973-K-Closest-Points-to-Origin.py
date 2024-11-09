3b8fe16c8dc852031c4abfbd2d89577599ab0944
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-09 08:44:48 -0500

    [973] [Time Beats: 41.76%] [Memory Beats: 85.10%] - LeetPush

--- 973-K-Closest-Points-to-Origin.py
+++ 973-K-Closest-Points-to-Origin.py
@@ -1,12 +1,10 @@
 import heapq
 import math
-
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        min_heap = []
+        max_heap = []
 
         for x,y in points:
-            heapq.heappush(min_heap, (-math.sqrt(x**2 + y**2), (x,y)))
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
-        return [points for (_,points) in min_heap]
+            heapq.heappush(max_heap, (-math.sqrt(x**2 + y**2), (x,y)))
+            if len(max_heap) > k: heapq.heappop(max_heap)
+        return [point for (_,point) in max_heap]
