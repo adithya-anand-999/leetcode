674a5d20ab22e7f7f427d1b303971c9f900d6282
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-08 23:12:23 -0500

    [542] [Time Beats: 69.19%] [Memory Beats: 98.86%] - LeetPush

--- 542-01-Matrix.py
+++ 542-01-Matrix.py
@@ -1,19 +1,21 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        starts = deque()
         m,n = len(mat), len(mat[0])
-        MAX_VAL = m*n
-        queue = deque()
-
+        max_val = m*n
         for i in range(m):
             for j in range(n):
-                if mat[i][j] == 0: queue.append((i,j))
-                else: mat[i][j] = MAX_VAL
-        while queue:
-            row,col = queue.popleft()
-            dir = [(1,0), (0,1), (-1,0), (0,-1)]
+                if mat[i][j] == 0: starts.append((i,j))
+                else: mat[i][j] = max_val
+        
+        while starts:
+            row,col = starts.popleft()
+
+            dir = [(-1,0), (0,-1), (1,0), (0,1)]
             for dr,dc in dir:
-                r,c = row+dr, col+dc
-                if 0<=r<m and 0<=c<n and mat[r][c] > 1+mat[row][col]:
+                r,c = row+dr,col+dc
+                if 0<= r < m and 0<= c < n and mat[r][c] > 1+mat[row][col]:
                     mat[r][c] = 1+mat[row][col]
-                    queue.append((r,c))
-        return mat
+                    starts.append((r,c))
+
+        return mat            
