de4017d954924c64af3cbaa4f4269ad2b3a5508c
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-09 21:59:22 -0500

    [15] [Time Beats: 12.03%] [Memory Beats: 98.74%] - LeetPush

--- 15-3Sum.py
+++ 15-3Sum.py
@@ -1,15 +1,19 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # res = set()
+        res = []
         n = len(nums)
-        res = set()
-        #you have to sort nums inorder to do binary search
         nums.sort()
+
         for i in range(n-2):
+            if i>0 and nums[i] == nums[i-1]: continue
             l,r = i+1,n-1
             while l<r:
-                sum = nums[i] + nums[l] + nums[r]
+                sum = nums[l] + nums[r] + nums[i]
                 if sum == 0:
-                    res.add((nums[i], nums[l], nums[r]))
+                    res.append((nums[l], nums[r], nums[i]))
+                    while (l+1)<r and nums[l] == nums[r]: l+=1
+                    while l<(r-1) and nums[r] == nums[r-1]: r-=1
                     l+=1
                     r-=1
                 elif sum < 0:
