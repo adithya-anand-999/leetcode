3e300960d887b367bbed0289a8353a661d242e7e
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-17 11:35:03 -0500

    [98] [Time Beats: 100.00%] [Memory Beats: 16.28%] - LeetPush

---
+++ 98-Validate-Binary-Search-Tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def help(node,min,max):
+            #base case returns True as we have made it to root without any errors so this level is good. 
+            if not node: return True
+            # if min > node.val > max
+            #the node cannot be past or equal the bounds 
+            if min >= node.val or node.val >= max: return False
+
+            left,right = help(node.left, min, node.val), help(node.right, node.val, max)
+            
+            if not left or not right: return False
+            return True
+        return help(root,-float('inf'),float('inf'))
