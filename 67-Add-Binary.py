c149d7fc9618ac92f05848b93889c67a62254117
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-18 20:28:20 -0500

    [67] [Time Beats: 51.09%] [Memory Beats: 20.44%] - LeetPush

--- 67-Add-Binary.py
+++ 67-Add-Binary.py
@@ -1,25 +1,28 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         a,b = list(a),list(b)
-        diff = abs(len(a) - len(b))
-        if len(a) < len(b):
-            a = ['0']*diff + a
+        la,lb = len(a),len(b)
+
+        if la < lb:
+            a = [0]*abs(la-lb) + a
         else:
-            b = ['0']*diff + b
+            b = [0]*abs(la-lb) + b
         
+        # print(a,b)
         carry = 0
-        for i in range(len(b)-1,-1,-1):
-            sum = int(a[i]) + int(b[i]) + carry
+        for i in range(len(a)-1, -1, -1):
+            sum = carry + int(a[i]) + int(b[i])
             if sum == 0:
                 a[i] = '0'
             elif sum == 1:
-                a[i] = '1'
                 carry = 0
-            elif sum == 2:
-                a[i] = '0'
-                carry = 1
-            else:
                 a[i] = '1'
+            elif sum == 2:
                 carry = 1
-        # if carry == 1: a = + a
-        return ''.join(a) if carry == 0 else str(carry)+''.join(a)
+                a[i] = '0'
+            else:
+                carry = 1
+                a[i] = '1'
+
+        if carry == 1: a = ['1'] + a
+        return ''.join(a)
