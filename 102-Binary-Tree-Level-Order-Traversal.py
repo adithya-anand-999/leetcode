af82ec79f3538550b09af2d2e09d1cde47c11b6e
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-19 12:37:34 -0500

    [102] [Time Beats: 100.00%] [Memory Beats: 15.21%] - LeetPush

--- 102-Binary-Tree-Level-Order-Traversal.py
+++ 102-Binary-Tree-Level-Order-Traversal.py
@@ -7,19 +7,16 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root: return []
-        prev_lvl = 0
         res = [[]]
-        queue = deque([(root, 0)])
-        #recursive traversal is for dfs, for bfs iterative is easier
+        prev = 0
+        queue = deque([(root,0)])
 
         while queue:
             node,lvl = queue.popleft()
-            if lvl != prev_lvl:
+            if lvl != prev: 
                 res.append([])
-                prev_lvl+=1
-            res[lvl].append(node.val)
+                prev+=1
+            res[prev].append(node.val)
             if node.left: queue.append((node.left, lvl+1))
             if node.right: queue.append((node.right, lvl+1))
         return res
-        
-        
