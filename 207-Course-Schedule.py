0f34c5c168dd0fc29e2ce7ff70ec0cdfe602af7a
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-23 14:58:41 -0500

    [207] [Time Beats: 88.55%] [Memory Beats: 28.41%] - LeetPush

--- 207-Course-Schedule.py
+++ 207-Course-Schedule.py
@@ -1,28 +1,23 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        courses = defaultdict(list)
-        for crs,pre in prerequisites:
-            courses[crs].append(pre)
+        prereqs = defaultdict(list)
+        for tar,pre in prerequisites:
+            prereqs[tar].append(pre)
         
-        path = set()
         visited = set()
-        def dfs(crs):
-            if crs in path: return False
-            if crs in visited: return True
+        path = set()
 
-#this is not a crs on the current path and we have not seen it before, so we are going to see it
-#its on the current path so add it to path            
+        def dfs(crs):
+            if crs in visited: return True
+            if crs in path: return False
+
             path.add(crs)
-#we visit by checking each of its prereqs            
-            for pre in courses[crs]:
-#if any prereq return False then we should return False, return False if we end up at a node seen on the current dfs path
-                if not dfs(pre): return False
-#the crs is good, so remove from the path and add it to visited as its good
+            for c in prereqs[crs]:
+                if not dfs(c): return False
             path.remove(crs)
             visited.add(crs)
-#the course is good so we return Ture
             return True
-        for crs in range(numCourses):
-#If any course returns false then we return false             
-            if not dfs(crs): return False
+        
+        for i in range(numCourses):
+            if not dfs(i): return False
         return True
