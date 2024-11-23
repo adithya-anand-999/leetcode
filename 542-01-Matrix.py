ce49869dd2e7afd98e103fe5d78877291f05e4b6
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-23 15:37:25 -0500

    [542] [Time Beats: 86.23%] [Memory Beats: 96.97%] - LeetPush

--- 542-01-Matrix.py
+++ 542-01-Matrix.py
@@ -1,21 +1,20 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        starts = deque()
-        m,n = len(mat), len(mat[0])
-        max_val = m*n
-        for i in range(m):
-            for j in range(n):
-                if mat[i][j] == 0: starts.append((i,j))
-                else: mat[i][j] = max_val
+        m,n = len(mat[0]),len(mat)
+        maxVal = 1+(m*n)
+
+        starts = deque([])
+        for r in range(n):
+            for c in range(m):
+                if mat[r][c] == 0: starts.append((r,c))
+                else: mat[r][c] = maxVal
         
+        dir = [(-1,0), (1,0), (0,-1), (0,1)]
         while starts:
-            row,col = starts.popleft()
-
-            dir = [(-1,0), (0,-1), (1,0), (0,1)]
+            r,c = starts.popleft()
             for dr,dc in dir:
-                r,c = row+dr,col+dc
-                if 0<= r < m and 0<= c < n and mat[r][c] > 1+mat[row][col]:
-                    mat[r][c] = 1+mat[row][col]
-                    starts.append((r,c))
-
-        return mat            
+                nr,nc = r+dr,c+dc
+                if nr>=0 and nr<n and nc>=0 and nc<m and mat[nr][nc] > 1+mat[r][c]:
+                    starts.append((nr,nc))
+                    mat[nr][nc] = 1+mat[r][c]
+        return mat
