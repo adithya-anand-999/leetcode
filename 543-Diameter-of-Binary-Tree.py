2e1cb0bcf02fa795a789b7a149d5e3021425d5f9
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-23 15:05:50 -0500

    [543] [Time Beats: 94.03%] [Memory Beats: 60.15%] - LeetPush

--- 543-Diameter-of-Binary-Tree.py
+++ 543-Diameter-of-Binary-Tree.py
@@ -6,14 +6,15 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        res = 0
+        maxLen = 0
 
         def help(node):
-            nonlocal res
+            nonlocal maxLen
 
             if not node: return 0
-            left,right = help(node.left),help(node.right)
-            res = max(res, left+right)
+            left,right = help(node.left), help(node.right)
+
+            maxLen = max(maxLen, left+right)
             return 1+max(left,right)
         help(root)
-        return res
+        return maxLen
