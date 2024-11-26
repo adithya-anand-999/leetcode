ea4e29d75bfd24e067aba1f558107b1c0fe64acd
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2024-11-26 11:54:31 -0500

    [994] [Time Beats: 100.00%] [Memory Beats: 5.98%] - LeetPush

--- 994-Rotting-Oranges.py
+++ 994-Rotting-Oranges.py
@@ -1,40 +1,24 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         q = deque()
-        n,m = len(grid), len(grid[0])
-        numFresh = 0
-        time = -1
-        dirs = [(-1,0), (1,0), (0,-1), (0,1)]
+        numFresh, time = 0, -1
+        n,m = len(grid),len(grid[0])
 
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 2:q.append((i,j))
-                elif grid[i][j] == 1:numFresh+=1
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c] == 2: q.append((r,c))
+                elif grid[r][c] == 1: numFresh+=1
         if not q and numFresh == 0: return 0
 
         while q:
-            for i in range(len(q)):
+            for _ in range(len(q)):
                 r,c = q.popleft()
-                for dr,dc in dirs:
+                for dr,dc in [(-1,0), (1,0), (0,1), (0,-1)]:
                     nr,nc = r+dr,c+dc
                     if nr>=0 and nr<n and nc>=0 and nc<m and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2 #make the fresh orange rotten
-                        q.append((nr,nc)) #add to our queue for next iteration as it will spread from here
-                        numFresh-=1 #decrement number of fresh oranges
+                        grid[nr][nc] = 2
+                        numFresh-=1
+                        q.append((nr,nc))
             time+=1
         if numFresh>0: return -1
         return time
-        #     qLen = len(q)
-        #     holder = deque()
-        #     while q:
-        #         r,c = q.popleft()
-        #         for dr,dc in dirs:
-        #             nr,nc = r+dr,c+dc
-        #             if nr>=0 and nr<n and nc>=0 and nc<m and grid[nr][nc] == 1:
-        #                 grid[nr][nc] = 2
-        #                 holder.append((nr,nc))
-        #                 numFresh-=1
-        #     q = holder
-        #     time+=1
-        # if numFresh > 0: return -1
-        # return time
