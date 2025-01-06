5da928b22dd67e8f8a398c97a7115df36492517c
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-05 18:37:56 -0600

    [110] [Time Beats: 20.93%] [Memory Beats: 9.32%] - LeetPush

--- 110-Balanced-Binary-Tree.py
+++ 110-Balanced-Binary-Tree.py
@@ -6,17 +6,12 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def help(node):
-            if not node:
-                return (True, 0)
-            left, right = help(node.left), help(node.right)
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            #is there a way to break out of all recrusion, and just clear the call stack. Once I see that balanced is false in any level of recursion. I can conclude that the entire tree is unbalanced, there would be no need for my to continue recusing. 
-            return (balanced, 1+max(left[1], right[1]))
-        return help(root)[0]
+        
+        def help(node,height):
+            if not node: return (True, 0)
+            left,right = help(node.left,height), help(node.right,height)
+            valid = left[0] and right[0] and abs(left[1]-right[1]) <= 1
+            height = max(left[1], right[1]) + 1
+            return (valid,height)
+        return help(root,0)[0]
 
-            # left2.left = (True, 2), left2.right = (True,1)
-            # left2 = (True,3)
-            # right2 = (True,1)
-            # root.left = left2
-            # root.right = right2
