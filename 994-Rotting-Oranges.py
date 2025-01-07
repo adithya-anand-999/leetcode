7ab4016a7f10045306b89b1971b86d8490c2cf47
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-06 18:46:56 -0600

    [994] [Time Beats: 82.45%] [Memory Beats: 23.78%] - LeetPush

--- 994-Rotting-Oranges.py
+++ 994-Rotting-Oranges.py
@@ -1,24 +1,32 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = deque()
-        numFresh, time = 0, -1
-        n,m = len(grid),len(grid[0])
+        rotten = deque()
+        n,m = len(grid), len(grid[0])
+        time = 0 # 1 or 0
+        dirs = [(-1,0), (1,0), (0,1), (0,-1)]
+        num_fresh = 0
 
         for r in range(n):
             for c in range(m):
-                if grid[r][c] == 2: q.append((r,c))
-                elif grid[r][c] == 1: numFresh+=1
-        if not q and numFresh == 0: return 0
+                if grid[r][c] == 2:
+                    rotten.append((r,c))
+                elif grid[r][c] == 1:
+                    num_fresh+=1
 
-        while q:
-            for _ in range(len(q)):
-                r,c = q.popleft()
-                for dr,dc in [(-1,0), (1,0), (0,1), (0,-1)]:
-                    nr,nc = r+dr,c+dc
-                    if nr>=0 and nr<n and nc>=0 and nc<m and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2
-                        numFresh-=1
-                        q.append((nr,nc))
-            time+=1
-        if numFresh>0: return -1
-        return time
+        if num_fresh == 0: return 0
+        
+        while rotten:
+            initial_rotten = len(rotten)
+            for i in range(initial_rotten):
+                r,c = rotten.popleft()
+                for dr,dc in dirs:
+                    fr,fc = dr+r,dc+c
+                    if fr >= 0 and fr < n and fc >= 0 and fc < m and grid[fr][fc] == 1:
+                        num_fresh-=1
+                        grid[fr][fc] = 2
+                        rotten.append((fr,fc))
+            if rotten: time+=1 # This means we have rotted something so we increment time, else nothing new rotten so prev time state equal to curr time state. 
+
+        
+        return time if num_fresh == 0 else -1
+                    
