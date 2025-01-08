6905ae54983b9a5287042fb9c01c9d32520c5cd7
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-08 14:15:24 -0600

    [57] [Time Beats: 50.24%] [Memory Beats: 44.41%] - LeetPush

--- 57-Insert-Interval.py
+++ 57-Insert-Interval.py
@@ -1,14 +1,16 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res = []
+        # if not intervals: return [newInterval]
         i = 0
+        res = []
         n = len(intervals)
-
-        while i<n and intervals[i][1] < newInterval[0]:
+        
+        while i < n and newInterval[0] > intervals[i][1]:
             res.append(intervals[i])
             i+=1
-        while i<n and intervals[i][0] <= newInterval[1]:
-            newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+        
+        while i < n and newInterval[1] >= intervals[i][0]: # you stop merging if intervals[i][0] happens after newInterval[1], aka newInterval ending
+            newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1],intervals[i][1])]
             i+=1
         res.append(newInterval)
         res+=intervals[i:]
