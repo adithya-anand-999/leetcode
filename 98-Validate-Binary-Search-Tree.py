e70e4cbe5264af0915d83a668fe0f40a3ebfe30c
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-08 09:38:18 -0600

    [98] [Time Beats: 100.00%] [Memory Beats: 10.77%] - LeetPush

--- 98-Validate-Binary-Search-Tree.py
+++ 98-Validate-Binary-Search-Tree.py
@@ -6,12 +6,13 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-
         def help(node, min, max):
             if not node: return True
-            if min >= node.val or node.val >= max: return False
-            left,right = help(node.left, min, node.val), help(node.right, node.val, max)
-            if not left or not right: return False
+            val = node.val
+            if min >= val or val >= max: return False
+
+            if not help(node.left, min, val) or not help(node.right, val, max):
+                return False
             return True
-        return help(root, -float('inf'), float('inf'))
         
+        return help(root, float('-inf'), float('inf'))
