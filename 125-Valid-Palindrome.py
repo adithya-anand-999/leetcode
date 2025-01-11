ae3d1cbb7157fca81021ebb83939e3d2f74c82ed
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-11 17:24:38 -0600

    [125] [Time Beats: 84.65%] [Memory Beats: 39.80%] - LeetPush

--- 125-Valid-Palindrome.py
+++ 125-Valid-Palindrome.py
@@ -1,14 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        valid = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
+        valid = 'abcdefghijklmnopqrstuvwxyz0123456789'
         l,r = 0,len(s)-1
         while l<r:
-            while l<r and s[l] not in valid:
-                l+=1
-            while l<r and s[r] not in valid:
-                r-=1
+            while (l+1)<=r and s[l] not in valid: l+=1
+            while l<=(r-1) and s[r] not in valid: r-=1
             if s[l] != s[r]: return False
             l+=1
             r-=1
