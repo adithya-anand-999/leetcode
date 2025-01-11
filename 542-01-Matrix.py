2a26dfeaa842b23b77e9bd7854f912aa96fa07c5
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-10 23:54:33 -0600

    [542] [Time Beats: 80.94%] [Memory Beats: 83.81%] - LeetPush

--- 542-01-Matrix.py
+++ 542-01-Matrix.py
@@ -1,20 +1,23 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        m,n = len(mat[0]),len(mat)
-        maxVal = 1+(m*n)
-
         starts = deque([])
-        for r in range(n):
-            for c in range(m):
-                if mat[r][c] == 0: starts.append((r,c))
-                else: mat[r][c] = maxVal
+        m,n = len(mat),len(mat[0])
+        max_val = m*n+1
+        dirs = [(-1,0), (1,0), (0,-1), (0,1)]
+
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] == 0:
+                    starts.append((r,c))
+                else:
+                    mat[r][c] = max_val
         
-        dir = [(-1,0), (1,0), (0,-1), (0,1)]
         while starts:
             r,c = starts.popleft()
-            for dr,dc in dir:
-                nr,nc = r+dr,c+dc
-                if nr>=0 and nr<n and nc>=0 and nc<m and mat[nr][nc] > 1+mat[r][c]:
-                    starts.append((nr,nc))
-                    mat[nr][nc] = 1+mat[r][c]
+            val = mat[r][c]
+            for dr,dc in dirs:
+                fr,fc = r+dr,c+dc
+                if fr>=0 and fr<m and fc>=0 and fc<n and mat[fr][fc] > val+1:
+                    starts.append((fr,fc))
+                    mat[fr][fc] = val+1
         return mat
