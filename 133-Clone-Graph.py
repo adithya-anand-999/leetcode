cf3e810281243512c9cd0949335b8c6b6a85b189
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-11 11:33:35 -0600

    [133] [Time Beats: 26.40%] [Memory Beats: 6.24%] - LeetPush

--- 133-Clone-Graph.py
+++ 133-Clone-Graph.py
@@ -10,15 +10,16 @@ from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node: return None
-        copies = {}
-        copies[node] = Node(node.val, [])
-        queue = deque([(node)])
+        copy = {}
+        copy[node.val] = Node(node.val, [])
+        q = deque([node])
 
-        while queue:
-            curr = queue.popleft()
+        while q:
+            curr = q.popleft()
             for n in curr.neighbors:
-                if n not in copies:
-                    copies[n] = Node(n.val, [])
-                    queue.append(n)
-                copies[curr].neighbors.append(copies[n])
-        return copies[node]
+                if n.val not in copy: #the key is node.val not node
+                    copy[n.val] = Node(n.val, [])
+                    q.append(n)
+                copy[curr.val].neighbors.append(copy[n.val])
+            print(copy)
+        return copy[node.val]
