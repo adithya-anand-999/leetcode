85f9443ef216d0c7a64703bc125dd17ca9ed8122
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-11 20:00:47 -0600

    [238] [Time Beats: 35.46%] [Memory Beats: 20.49%] - LeetPush

--- 238-Product-of-Array-Except-Self.py
+++ 238-Product-of-Array-Except-Self.py
@@ -4,8 +4,10 @@ class Solution:
         prefix = [1]*n
         suffix = [1]*n
 
-        for i in range(1,n): prefix[i] = nums[i-1]*prefix[i-1]
-        for i in range(n-2,-1,-1): suffix[i] = nums[i+1]*suffix[i+1]
-
-        for i in range(n):nums[i] = prefix[i]*suffix[i]
+        for i in range(1,n):
+            prefix[i] = prefix[i-1]*nums[i-1]
+        for i in range(n-2,-1,-1):
+            suffix[i] = suffix[i+1]*nums[i+1]
+        for i in range(n):
+            nums[i] = prefix[i]*suffix[i]
         return nums
