11a1e386d910abc7c6580789c64f7a192de4953a
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-11 18:04:14 -0600

    [208] [Time Beats: 78.58%] [Memory Beats: 21.29%] - LeetPush

--- 208-Implement-Trie-(Prefix-Tree).py
+++ 208-Implement-Trie-(Prefix-Tree).py
@@ -1,34 +1,36 @@
 class Node:
-    def __init__(self, val=0):
-        self.val = val
-        self.neighbors = {}
-        self.end = False
+    def __init__(self, value):
+        self.value = value
+        self.children = {} # dict so you can search for the char as key and the Node for that char as the value.
+        self.end = False #You set to false as it means its not a complete word seen, when you have seen a complete word you set this to True
 
 class Trie:
 
     def __init__(self):
-        self.tree = Node()
+        self.tree = Node('*')
 
     def insert(self, word: str) -> None:
         curr = self.tree
+        # print(word)
         for c in word:
-            if c not in curr.neighbors:
-                curr.neighbors[c] = Node(c)
-            curr = curr.neighbors[c]
+            # print(c)
+            if c not in curr.children:
+                curr.children[c] = Node(c)
+            curr = curr.children[c]
         curr.end = True
 
     def search(self, word: str) -> bool:
         curr = self.tree
         for c in word:
-            if c not in curr.neighbors: return False
-            curr = curr.neighbors[c]
-        return curr.end
+            if c not in curr.children: return False
+            curr = curr.children[c]
+        return curr.end  # If the last node has its end set to true the word exists and not just a prefix. 
 
     def startsWith(self, prefix: str) -> bool:
         curr = self.tree
         for c in prefix:
-            if c not in curr.neighbors: return False
-            curr = curr.neighbors[c]
+            if c not in curr.children: return False
+            curr = curr.children[c]
         return True
 
 
