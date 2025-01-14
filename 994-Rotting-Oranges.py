c12442aa10d69ad046b3c8fb853dd3998a054266
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-13 20:17:29 -0600

    [994] [Time Beats: 82.79%] [Memory Beats: 12.83%] - LeetPush

--- 994-Rotting-Oranges.py
+++ 994-Rotting-Oranges.py
@@ -1,32 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rotten = deque()
-        n,m = len(grid), len(grid[0])
-        time = 0 # 1 or 0
-        dirs = [(-1,0), (1,0), (0,1), (0,-1)]
+        m,n = len(grid),len(grid[0])
         num_fresh = 0
+        time = 0
+        starts = deque([])
+        dirs = [(-1,0), (1,0), (0,-1), (0,1)]
 
-        for r in range(n):
-            for c in range(m):
-                if grid[r][c] == 2:
-                    rotten.append((r,c))
-                elif grid[r][c] == 1:
-                    num_fresh+=1
-
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 1: num_fresh+=1
+                elif grid[r][c] == 2: starts.append((r,c))
+        
+        # if not starts: return -1
         if num_fresh == 0: return 0
-        
-        while rotten:
-            initial_rotten = len(rotten)
-            for i in range(initial_rotten):
-                r,c = rotten.popleft()
-                for dr,dc in dirs:
-                    fr,fc = dr+r,dc+c
-                    if fr >= 0 and fr < n and fc >= 0 and fc < m and grid[fr][fc] == 1:
-                        num_fresh-=1
-                        grid[fr][fc] = 2
-                        rotten.append((fr,fc))
-            if rotten: time+=1 # This means we have rotted something so we increment time, else nothing new rotten so prev time state equal to curr time state. 
 
-        
+        while starts:
+            rotten_number = len(starts)
+            for _ in range(rotten_number):
+                r,c = starts.popleft()
+                for dr,dc in dirs:
+                    fr,fc = r+dr,c+dc
+                    if fr>=0 and fr<m and fc>=0 and fc<n and grid[fr][fc] == 1:
+                        grid[fr][fc] = 2
+                        num_fresh-=1
+                        starts.append((fr,fc))
+            if starts: time+=1
         return time if num_fresh == 0 else -1
-                    
