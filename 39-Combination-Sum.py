e5e6af257252c0aac7fe3e3bf6c5f0e978f48964
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-13 19:10:44 -0600

    [39] [Time Beats: 91.18%] [Memory Beats: 29.31%] - LeetPush

--- 39-Combination-Sum.py
+++ 39-Combination-Sum.py
@@ -4,20 +4,16 @@ class Solution:
         n = len(candidates)
 
         def help(val, trace, start):
-            # print(trace)
             if val == 0:
-                # The issue might be that the same trace is being modified, they all point to the same trace list. 
                 res.append(trace[:])
                 return
             if val < 0:
-                return 
+                return
             
-            for i in range(start, n):
-                c = candidates[i]
-                if c <= val:
-                    trace.append(c)
-                    help(val-c, trace, i)
+            for i in range(start,n):
+                if candidates[i] <= val:
+                    trace.append(candidates[i])
+                    help(val-candidates[i], trace, i)
                     trace.pop()
-
-        help(target, [], 0)
+        help(target,[],0)
         return res
