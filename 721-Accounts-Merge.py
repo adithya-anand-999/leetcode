2caeffb3971d25a2b9690a68fb59cd2b9ee08890
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-18 12:57:57 -0600

    [721] [Time Beats: 68.27%] [Memory Beats: 49.25%] - LeetPush

--- 721-Accounts-Merge.py
+++ 721-Accounts-Merge.py
@@ -1,16 +1,14 @@
 class UnionFind:
-    def __init__(self, n):
+    def __init__ (self,n):
         self.parent = list(range(n))
         self.rank = [0]*n
-    def find(self,x):
+    
+    def find (self, x):
         if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x]) 
-            # path comphrehension as the parent of the node should be absolute, all nodes in a group only go up 1 level. 
-            # If I called find on each node in a group I would reduce the height of the group to 1 as all the nodes that are not parent would point directlly to parent and not intermediate nodes. 
-            # We dont update rank as rank is just an approximation of the height of the group, used only for the union method. 
+            self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
-    def union(self,x,y):
-        r_x, r_y = self.find(x), self.find(y)
+    def union (self,x,y):
+        r_x,r_y = self.find(x),self.find(y)
 
         if r_x != r_y:
             if self.rank[r_x] > self.rank[r_y]:
@@ -18,30 +16,29 @@ class UnionFind:
             elif self.rank[r_x] < self.rank[r_y]:
                 self.parent[r_x] = r_y
             else:
-                self.parent[r_y] = r_x
-                self.rank[r_x]+=1
+                self.parent[r_x] = r_y
+                self.rank[r_y]+=1
             return True
         return False
-
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         uf = UnionFind(len(accounts))
-        email_to_acc = {}
 
+        email_map = {}
         for i,a in enumerate(accounts):
             for e in a[1:]:
-                if e in email_to_acc:
-                    uf.union(i,email_to_acc[e]) #The sets are indicies of the accounts
+                if e in email_map:
+                    uf.union(i, email_map[e])
                 else:
-                    email_to_acc[e] = i
-
-        res = defaultdict(list)
-        for e,i in email_to_acc.items():
+                    email_map[e] = i
+        
+        account_map = defaultdict(list)
+        for e,i in email_map.items():
             parent = uf.find(i)
-            res[parent].append(e) #dont have the name of account at index as key now, as names can be shared between accounts
-        print(res)
-        res_list = []
-        for i,emails in res.items():
-            res_list.append([accounts[i][0]] + sorted(emails))
-        return res_list
+            account_map[parent].append(e)
+        
+        res = []
+        for i,emails in account_map.items():
+            res.append([accounts[i][0]] + sorted(emails))
+        return res
