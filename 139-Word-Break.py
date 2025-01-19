3ca4232df9a03b509e9c4e4abffd11069ba08b93
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-19 14:49:21 -0600

    [139] [Time Beats: 27.14%] [Memory Beats: 39.54%] - LeetPush

--- 139-Word-Break.py
+++ 139-Word-Break.py
@@ -8,10 +8,6 @@ class Solution:
         for i in range(1,n+1):
             for j in range(i):
                 if dp[j] and s[j:i] in wordSet:
-                #window size if i chars
-                #dp[J] checks if first j chars can be made, j<i as its range(i) so never i
-                #s[j:i] in wordSet checks if chars after first j inside wordSet.
-                #If both true then dp[i] can be made fully.
                     dp[i] = True
                     break
         return dp[n]
