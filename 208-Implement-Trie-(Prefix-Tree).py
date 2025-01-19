224be6fb96c109b70649e06a4028b9f1dde02bf8
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-19 13:40:42 -0600

    [208] [Time Beats: 67.83%] [Memory Beats: 27.97%] - LeetPush

--- 208-Implement-Trie-(Prefix-Tree).py
+++ 208-Implement-Trie-(Prefix-Tree).py
@@ -1,33 +1,32 @@
 class Node:
-    def __init__(self, value):
-        self.value = value
-        self.children = {} # dict so you can search for the char as key and the Node for that char as the value.
-        self.end = False #You set to false as it means its not a complete word seen, when you have seen a complete word you set this to True
+    def __init__(self,val='*'):
+        self.val = val
+        self.children = {}
+        self.end = False
+    
 
 class Trie:
 
     def __init__(self):
-        self.tree = Node('*')
+        self.root = Node()
 
     def insert(self, word: str) -> None:
-        curr = self.tree
-        # print(word)
+        curr = self.root
         for c in word:
-            # print(c)
             if c not in curr.children:
                 curr.children[c] = Node(c)
             curr = curr.children[c]
         curr.end = True
 
     def search(self, word: str) -> bool:
-        curr = self.tree
+        curr = self.root
         for c in word:
             if c not in curr.children: return False
             curr = curr.children[c]
-        return curr.end  # If the last node has its end set to true the word exists and not just a prefix. 
+        return curr.end
 
     def startsWith(self, prefix: str) -> bool:
-        curr = self.tree
+        curr = self.root
         for c in prefix:
             if c not in curr.children: return False
             curr = curr.children[c]
