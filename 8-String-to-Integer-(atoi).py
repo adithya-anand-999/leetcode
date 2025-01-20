ebdcaf528872115a87a6af269721d0739fdb4ca7
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-20 13:10:12 -0600

    [8] [Time Beats: 40.44%] [Memory Beats: 16.23%] - LeetPush

--- 8-String-to-Integer-(atoi).py
+++ 8-String-to-Integer-(atoi).py
@@ -3,23 +3,18 @@ class Solution:
         pos = True
         r = 0
         n = len(s)
-        # res = 0
         valid = '0123456789'
 
-#whitespace
-        while r<n and s[r].isspace(): r+=1
+        while r<n and s[r] == \ \: r+=1
         
-#sign
-        if r<n and s[r] == '-': 
+        if r<n and s[r] == '+': r+=1
+        elif r<n and s[r] == '-': 
             pos = False
             r+=1
-        elif r<n and s[r] == '+': r+=1
-
-#leading 0   
+        
         while r<n and s[r] == '0': r+=1
 
         l = r
-#char check
         while r<n and s[r] in valid: r+=1
 
         if abs(l-r) > 0:
