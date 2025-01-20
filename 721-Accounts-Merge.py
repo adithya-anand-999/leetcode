8f2bc05a2446fd58bc72e53fe70147206b5ca658
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-20 13:20:16 -0600

    [721] [Time Beats: 79.40%] [Memory Beats: 67.22%] - LeetPush

--- 721-Accounts-Merge.py
+++ 721-Accounts-Merge.py
@@ -1,30 +1,29 @@
 class UnionFind:
     def __init__(self,n):
-        self.parent = list(range(n))
+        self.pars = list(range(n))
         self.rank = [0]*n
     def find(self,x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+        if self.pars[x] != x:
+            self.pars[x] = self.find(self.pars[x])
+        return self.pars[x]
     def union(self,x,y):
         r_x,r_y = self.find(x),self.find(y)
 
         if r_x != r_y:
-            if self.rank[r_x] > self.rank[r_y]:
-                self.parent[r_y] = r_x
-            elif self.rank[r_x] < self.rank[r_y]:
-                self.parent[r_x] = r_y
+            if self.rank[r_x] < self.rank[r_y]:
+                self.pars[r_x] = r_y
+            elif self.rank[r_x] > self.rank[r_y]:
+                self.pars[r_y] = r_x
             else:
-                self.parent[r_x] = r_y
+                self.pars[r_x] = r_y
                 self.rank[r_y]+=1
             return True
-        return False
-
+        return False 
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         uf = UnionFind(len(accounts))
-
+        
         email_map = {}
         for i,a in enumerate(accounts):
             for e in a[1:]:
@@ -32,11 +31,11 @@ class Solution:
                     uf.union(i,email_map[e])
                 else:
                     email_map[e] = i
-
+        
         acc_map = defaultdict(list)
         for e,i in email_map.items():
             par = uf.find(i)
-            acc_map[par].append(e)    
+            acc_map[par].append(e)
         
         res = []
         for i,emails in acc_map.items():
