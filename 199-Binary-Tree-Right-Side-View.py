836ee32e3bd0af6aecc3966e54839c56aa26d072
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-22 12:08:20 -0600

    [199] [Time Beats: 100.00%] [Memory Beats: 10.27%] - LeetPush

--- 199-Binary-Tree-Right-Side-View.py
+++ 199-Binary-Tree-Right-Side-View.py
@@ -6,21 +6,19 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        #for every level of the tree get the rightmost node
-        #so output == levels of the tree. 
-        # root is always included 
-
         if not root: return []
-        res = [[]]
         level = 0
+        res = [[]]
         q = deque([(root,0)])
 
         while q:
-            curr,curr_level = q.popleft()
-            if curr_level != level: 
+            curr,lvl = q.popleft()
+            if lvl != level:
                 res.append([])
                 level+=1
             res[level].append(curr.val)
-            if curr.left: q.append((curr.left, curr_level+1))
-            if curr.right: q.append((curr.right, curr_level+1))
+
+            if curr.left: q.append((curr.left,lvl+1))
+            if curr.right: q.append((curr.right,lvl+1))
+        
         return [x[-1] for x in res]
