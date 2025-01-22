ae9cde8f0b4a7ab19f8c003c95c5b95161e9fe58
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-22 11:08:04 -0600

    [5] [Time Beats: 94.02%] [Memory Beats: 25.97%] - LeetPush

--- 5-Longest-Palindromic-Substring.py
+++ 5-Longest-Palindromic-Substring.py
@@ -1,20 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-
         def expand(l,r):
             while l>=0 and r<n and s[l] == s[r]:
                 l-=1
                 r+=1
-            return (l+1,r)
-        
+            return (l+1,r-1)
+
         start,end = 0,0
         for i in range(n):
             oL,oR = expand(i,i)
             eL,eR = expand(i,i+1)
 
-            if oR-oL > end-start:
-                start,end = oL,oR
-            if eR-eL > end-start:
-                start,end = eL,eR
-        return s[start:end]
+            if oR-oL>end-start: start,end = oL,oR
+            if eR-eL>end-start: start,end = eL,eR
+        return s[start:end+1]  
