cb428332082a577357820eb29d25dfc6f1015836
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-22 11:18:07 -0600

    [994] [Time Beats: 83.49%] [Memory Beats: 59.49%] - LeetPush

--- 994-Rotting-Oranges.py
+++ 994-Rotting-Oranges.py
@@ -1,28 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m,n = len(grid),len(grid[0])
-        num_fresh = 0
         time = 0
+        fresh = 0
         starts = deque([])
-        dirs = [(-1,0), (1,0), (0,-1), (0,1)]
+        m,n = len(grid),len(grid[0])
+        dirs = [(-1,0),(1,0),(0,1),(0,-1)]
 
         for r in range(m):
             for c in range(n):
-                if grid[r][c] == 1: num_fresh+=1
+                if grid[r][c] == 1: fresh+=1
                 elif grid[r][c] == 2: starts.append((r,c))
         
-        # if not starts: return -1
-        if num_fresh == 0: return 0
+        if not starts and fresh>0: return -1
 
         while starts:
-            rotten_number = len(starts)
-            for _ in range(rotten_number):
+            num = len(starts)
+            for _ in range(num):
                 r,c = starts.popleft()
                 for dr,dc in dirs:
-                    fr,fc = r+dr,c+dc
-                    if fr>=0 and fr<m and fc>=0 and fc<n and grid[fr][fc] == 1:
-                        grid[fr][fc] = 2
-                        num_fresh-=1
-                        starts.append((fr,fc))
+                    nr,nc = r+dr,c+dc
+                    if nr>=0 and nr<m and nc>=0 and nc<n and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        starts.append((nr,nc))
+                        fresh-=1
             if starts: time+=1
-        return time if num_fresh == 0 else -1
+        
+        return time if fresh==0 else -1
