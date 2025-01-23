8cce2c0c79fcd201f39e0f5afaecde49bc8ebbb4
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-23 11:16:30 -0600

    [8] [Time Beats: 40.23%] [Memory Beats: 57.80%] - LeetPush

--- 8-String-to-Integer-(atoi).py
+++ 8-String-to-Integer-(atoi).py
@@ -1,26 +1,29 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        pos = True
         r = 0
         n = len(s)
+        pos = True
         valid = '0123456789'
 
-        while r<n and s[r] == \ \: r+=1
-        
-        if r<n and s[r] == '+': r+=1
-        elif r<n and s[r] == '-': 
+        while r<n and s[r] == ' ':r+=1
+
+        if r<n and s[r] == '-':
             pos = False
             r+=1
+        elif r<n and s[r] == '+':
+            r+=1
         
-        while r<n and s[r] == '0': r+=1
-
+        while r<n and s[r] == '0':r+=1
+        
         l = r
         while r<n and s[r] in valid: r+=1
 
-        if abs(l-r) > 0:
+        if r-l>0:
             num = int(s[l:r]) if pos else -int(s[l:r])
             if num > 2147483647: return 2147483647
             if num < -2147483648: return -2147483648
             return num
         else:
             return 0
+
+
