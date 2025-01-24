9e33807d86c2992e810d8ec25893b643f8ab8c5a
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-24 11:54:06 -0600

    [33] [Time Beats: 100.00%] [Memory Beats: 14.68%] - LeetPush

--- 33-Search-in-Rotated-Sorted-Array.py
+++ 33-Search-in-Rotated-Sorted-Array.py
@@ -3,13 +3,13 @@ class Solution:
         l,r = 0,len(nums)-1
 
         while l<=r:
-            m = (r+l)//2
-            if nums[m] == target: return m
-
-            if nums[m]-nums[l]>=0:
-                if nums[l]<=target<nums[m]: r = m-1
-                else: l = m+1
+            mid = (l+r)//2
+            if nums[mid] == target: return mid
+            
+            if nums[mid] >= nums[l]:
+                if nums[l] <= target < nums[mid]: r = mid-1
+                else: l = mid+1
             else:
-                if nums[m]<target<=nums[r]: l = m+1
-                else: r = m-1
+                if nums[mid] < target <= nums[r]: l = mid+1
+                else: r = mid-1
         return -1
