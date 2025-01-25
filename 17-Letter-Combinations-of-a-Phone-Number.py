7f5e7ba9fa273abd1fe9e50101d34ee54d0a2fff
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-25 10:30:27 -0600

    [17] [Time Beats: 100.00%] [Memory Beats: 45.62%] - LeetPush

--- 17-Letter-Combinations-of-a-Phone-Number.py
+++ 17-Letter-Combinations-of-a-Phone-Number.py
@@ -1,31 +1,28 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits: return[]
-        res = []
         n = len(digits)
-        # I need a mapping of the digit to the letters it corresponds too. 
-        # I am going to use len to see which digit I am on, I can index into my mapping by reading the char at index len in digits
-        # use a start variable to see which digit to read. But I actually dont need this, a simple loop should suffice. 
+        res = []
         mapping = {
-            2: ['a','b','c'],
-            3: ['d','e','f'],
-            4: ['g','h','i'],
-            5: ['j','k','l'],
-            6: ['m','n','o'],
-            7: ['p','q','r','s'],
-            8: ['t','u','v'],
-            9: ['w','x','y','z']
+            '2': ['a','b','c'],
+            '3': ['d','e','f'],
+            '4': ['g','h','i'],
+            '5': ['j','k','l'],
+            '6': ['m','n','o'],
+            '7': ['p','q','r','s'],
+            '8': ['t','u','v'],
+            '9': ['w','x','y','z']
         }
 
-        def help(trace,length):
-            if length == n: 
+        def geko(trace,idx):
+            nonlocal n
+            if idx == n: 
                 res.append(''.join(trace))
                 return
             
-            for c in mapping[int(digits[length])]:
-                trace.append(c)
-                help(trace,length+1)
+            for d in mapping[digits[idx]]:
+                trace.append(d)
+                geko(trace,idx+1)
                 trace.pop()
-        help([],0)
+        geko([],0)
         return res
-
