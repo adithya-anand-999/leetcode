a88928265d43581253453e061811db2e2da58dae
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-25 10:36:14 -0600

    [5] [Time Beats: 92.58%] [Memory Beats: 47.74%] - LeetPush

--- 5-Longest-Palindromic-Substring.py
+++ 5-Longest-Palindromic-Substring.py
@@ -6,12 +6,12 @@ class Solution:
                 l-=1
                 r+=1
             return (l+1,r-1)
-
-        start,end = 0,0
+        
+        l,r = 0,0
         for i in range(n):
             oL,oR = expand(i,i)
             eL,eR = expand(i,i+1)
 
-            if oR-oL>end-start: start,end = oL,oR
-            if eR-eL>end-start: start,end = eL,eR
-        return s[start:end+1]  
+            if eR-eL>r-l: l,r = eL,eR
+            if oR-oL>r-l: l,r = oL,oR
+        return s[l:r+1]
