ecb728289e9c7f5955d94270b5551a2985b192da
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-25 18:18:26 -0600

    [310] [Time Beats: 32.32%] [Memory Beats: 83.36%] - LeetPush

--- 310-Minimum-Height-Trees.py
+++ 310-Minimum-Height-Trees.py
@@ -2,33 +2,20 @@ class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n == 1: return[0]
 
-        # adjacency list, storing edges in a hashmap. Key is the node, values the connections it has. 
-        graph = defaultdict(list)
+        tree = defaultdict(list)
         for a,b in edges:
-            graph[a].append(b)
-            graph[b].append(a)
+            tree[a].append(b)
+            tree[b].append(a)
         
-        # leaves are keys in graph that have a value of length 1, meaning 1 connection.
-        leaves = deque([i for i in range(n) if len(graph[i]) == 1])\\
-
-        remaining_nodes = n
-        while remaining_nodes > 2:
-            leaves_count = len(leaves)
-            remaining_nodes -= leaves_count
-            for _ in range(leaves_count):
-                leaf =  leaves.popleft()
-                # why loop over all neighbors of the leaf node, there can only be 1 neightbor else it would not be a leaf node or included in leaves.
-                n = graph[leaf][0]
-                # del graph[leaf]
-                graph[n].remove(leaf)
-                if len(graph[n]) == 1: leaves.append(n)
-
-
-
-                #remove the leaf from the graph
-                # for n in graph[leaf]:
-                #     graph[n].remove(leaf)
-                #     # if the parent of the leaf node (n) now becomes a leaf node add it to the leaf node queue
-                #     if len(graph[n]) == 1:
-                #         leaves.append(n)
+        leaves = deque([i for i in range(n) if len(tree[i]) == 1])
+        
+        remains = n
+        while remains>2:
+            num_leaves = len(leaves)
+            remains-=num_leaves
+            for _ in range(num_leaves):
+                leaf = leaves.popleft()
+                n = tree[leaf][0]
+                tree[n].remove(leaf)
+                if len(tree[n]) == 1: leaves.append(n)
         return list(leaves)
