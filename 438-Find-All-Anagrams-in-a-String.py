4406132cdfd4c0d8abb084b18f7de8fdaad636ec
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-01-26 23:24:23 -0600

    [438] [Time Beats: 66.61%] [Memory Beats: 40.73%] - LeetPush

--- 438-Find-All-Anagrams-in-a-String.py
+++ 438-Find-All-Anagrams-in-a-String.py
@@ -1,34 +1,19 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        pL = len(p)
-        sL = len(s)
-
-        if sL<pL: return []
-
+        pl = len(p)
+        sl = len(s)
+        if sl < pl: return []
 
         res = []
-        window = defaultdict(int)
         freq_p = defaultdict(int)
-
-        for i,c in enumerate(p): 
+        window = defaultdict(int)
+        for i,c in enumerate(p):
             freq_p[c]+=1
             window[s[i]]+=1
-
         if window == freq_p: res.append(0)
-        # would storing the range in a variable be better computatianlly as each time the loop runs it would have to evaluate the expression
-        for i in range(pL,sL):
+        for i in range(pl,sl):
             window[s[i]]+=1
-            window[s[i-pL]]-=1
-            if window[s[i-pL]]==0: del window[s[i-pL]]
-            if window == freq_p: res.append(i-pL+1)
-
-        # i = 0
-        # while i<(sL-pL):
-        #     if window == freq_p: res.append(i)
-        #     window[s[i]]-=1
-        #     window[s[i+pL]]+=1
-        #     i+=1
-        # print(i)
-        # if window == freq_p: res.append(i)
-
+            window[s[i-pl]]-=1
+            if not window[s[i-pl]]: del window[s[i-pl]]
+            if window == freq_p: res.append(i-pl+1)
         return res
