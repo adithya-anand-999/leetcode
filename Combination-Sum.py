88cbf216882af1b4f6545e54637766efb18c00ca
Adithya Anand <37714224+adithya-anand-999@users.noreply.github.com>
2025-08-15 21:12:48 -0400

    [39] [Time Beats: 85.55%] [Memory Beats: 87.78%] - LeetPush

--- Combination-Sum.py
+++ Combination-Sum.py
@@ -1,15 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        def help(tot, trace, start):
-            if tot == target: 
+        can_len = len(candidates)
+        def help (start, trace, curr_sum):
+            if curr_sum == target:
                 res.append(trace[:])
                 return
-            if tot > target: return
-            for i,c in enumerate(candidates[start:]):
-                if c <= target-tot:
-                    trace.append(c)
-                    help(tot+c, trace, start+i)
-                    trace.pop()
+            # if curr_sum > target:
+            #     return
+
+            for i in range (start, can_len):
+                if candidates[i]+curr_sum <= target:
+                    # trace.append(candidates[i])
+                    help(i,trace+[candidates[i]],candidates[i]+curr_sum)
         help(0,[],0)
-        return res
+        return res 
